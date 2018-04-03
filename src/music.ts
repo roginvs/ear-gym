d07ccde02ccdb9eda1310177ab8f61ca27185b57
Vasilii Rogin <vasya@rogin.ru>
2018-04-03 13:22:47 +0300

    Updated media list

--- src/music.ts
+++ src/music.ts
@@ -4,16 +4,20 @@ export type MusicType = 'music' | 'piano' | 'voice';
 
 export function musicList(type: MusicType) {
     return type ==='music' ? [
-        "EIHWAZ - Delusion.mp3",
-        "Magni Animi Viri - Heroes.mp3",
-        "Marty Friedman - Trance.mp3",
-        "Power Tale - Guest.mp3",
-        "rock&opera - queen of night.mp3"
+        "pi-001.mp3",
+        "pi-002.mp3",
+        "pi-003.mp3",
+        "pi-004.mp3",
+        "pi-005.mp3",
+        "pi-006.mp3",
+        "pi-007.mp3",
+        "pi-008.mp3",
+        "pi-009.mp3",
+        "pi-010.mp3",        
     ] : type === 'piano' ? [
         "Joe Hisaishi - Howl's Moving Castle.mp3",
         "Kevin MacLeod - Merry Go.mp3"
-    ] : type === 'voice' ? [
-        "Van Canto - Wishmaster.mp3",
+    ] : type === 'voice' ? [        
         "Smeshariki - kuzinatra.mp3"
     ] : assertNever(type);
 };
