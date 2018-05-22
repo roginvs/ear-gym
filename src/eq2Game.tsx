36f71a1f9b7884ea62b5ebc2d3d345ae224565f5
Vasilii Rogin <vasya@rogin.ru>
2018-05-22 20:21:29 +0300

    Steps with eq2

--- src/eq2Game.tsx
+++ src/eq2Game.tsx
@@ -5,70 +5,123 @@ import { assertNever, range } from "./utils";
 import { Game, GameStageProps } from "./game";
 
 function lvlInfo(level: number) {
-const lvlInfoData: {
-    bandsTotal: number,
-    bandsAltered: number,
-    dbStep: number
-}[] = [{
-    bandsTotal: 10,
-    bandsAltered: 2,
-    dbStep: 3
-}];
-    return lvlInfoData[level - 1]
+    const lvlInfoData: {
+        bandsTotal: number;
+        bandsAltered: number;
+        dbStep: number;
+    }[] = [
+        {
+            bandsTotal: 10,
+            bandsAltered: 2,
+            dbStep: 3
+        }
+    ];
+    return lvlInfoData[level - 1];
 }
 
 interface EQ2GameState {
     correctDbs: number[];
     userDbs: number[];
 }
-class EQ2Game extends React.Component<GameStageProps,EQ2GameState>{
-    bands = range(0, lvlInfo(this.props.level).bandsTotal);
-    state = (() => {        
-        return {
-            correctDbs: this.bands.map((x, id) => id % 2 ? 0 : 6),
-            userDbs: this.bands.map(x => 0),
-        }
-    })();
 
+const MAX_DB = 12;
+
+class EQ2Game extends React.Component<GameStageProps, EQ2GameState> {
+    bands = range(0, lvlInfo(this.props.level).bandsTotal);
+    state = (() => {
+        return {
+            correctDbs: this.bands.map((x, id) => (id % 2 ? 0 : 6)),
+            userDbs: this.bands.map(x => 0)
+        };
+    })();
 
     fxes = this.bands.map(x => this.props.audioCtx.createBiquadFilter());
     componentDidUpdate() {
         this.updateFx();
     }
     componentDidMount() {
-        this.updateFx();                
+        this.updateFx();
         const gainNode = this.props.audioCtx.createGain();
         gainNode.gain.setValueAtTime(0.8, 0);
         this.props.srcAudio.connect(gainNode);
-        gainNode.connect(this.fxes[0]);        
-        this.fxes.map((fx, id) => id !== this.fxes.length -1 ? 
-        this.fxes[id].connect(this.fxes[id+1]) : this.fxes[id].connect(this.props.audioCtx.destination));
+        gainNode.connect(this.fxes[0]);
+        this.fxes.map(
+            (fx, id) =>
+                id !== this.fxes.length - 1
+                    ? this.fxes[id].connect(this.fxes[id + 1])
+                    : this.fxes[id].connect(this.props.audioCtx.destination)
+        );
 
         const minFreq = 100;
         const maxFreq = 12800;
         const max2Power = Math.log2(maxFreq / minFreq);
         const qStep = 2 ** (max2Power / this.fxes.length);
         this.fxes.map((fx, id) => {
-            fx.type = "peaking";            
-            const freq = minFreq* qStep ** id;
+            fx.type = "peaking";
+            const freq = minFreq * qStep ** id;
             fx.frequency.setValueAtTime(freq, 0);
             fx.Q.setValueAtTime(qStep, 0); // Maybe divide by two?
-            console.info(`id=${id} freq=${freq} qStep=${qStep}`)
+            console.info(`id=${id} freq=${freq} qStep=${qStep}`);
         });
     }
 
-    updateFx() {        
-        const eqGains = this.props.fxOn ? this.state.userDbs : this.state.correctDbs;
+    updateFx() {
+        const eqGains = this.props.fxOn
+            ? this.state.userDbs
+            : this.state.correctDbs;
         console.info(eqGains);
         this.fxes.map((fx, id) => fx.gain.setValueAtTime(eqGains[id], 0));
     }
 
-    
     componentWillUnmount() {
         this.fxes.map(fx => fx.disconnect());
     }
     render() {
-        return <div>TODO</div>
+        return (
+            <div>
+                <div className="my-1 text-center">
+                    <button className="btn btn-secondary text-dark">
+                        check answer
+                    </button>
+                </div>
+                <div className="py-2">
+                    <div
+                        className="bg-secondary rounded mx-2"
+                        style={{
+                            position: "relative",
+                            height: "9em"
+                        }}
+                    >
+                        {this.bands.map((band, id) => (
+                            <input
+                                key={id}
+                                value={this.state.userDbs[id]}
+                                type="range"
+                                min={-MAX_DB}
+                                max={MAX_DB}
+                                onChange={e => {
+                                    this.state.userDbs[id] = parseInt(
+                                        e.target.value
+                                    );
+                                    this.forceUpdate();
+                                    this.updateFx();
+                                }}
+                                style={{
+                                    position: "absolute",
+                                    //top: "50%",
+                                    left: `${100 * (id+1) / (this.bands.length + 1)}%`,
+                                    transformOrigin: "left top",
+                                    width: "8em",
+                                    height: "1em",
+                                    top: "8.5em",
+                                    transform: "rotate(270deg) translate(0%, -50%)",
+                                }}
+                            />
+                        ))}
+                    </div>
+                </div>
+            </div>
+        );
     }
 }
 
@@ -79,5 +132,5 @@ export const EQ2_GAME: Game = {
     maxLevels: 8,
     eachStageFxOff: true,
     // levelInfo?: (levelNumber: number) => JSX.Element,
-    stage: props => <EQ2Game {...props}/>
-}
+    stage: props => <EQ2Game {...props} />
+};
